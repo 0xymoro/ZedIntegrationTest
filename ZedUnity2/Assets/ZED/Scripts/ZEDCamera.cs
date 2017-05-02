4019315d8da49189f073b659008009e5b692d269
Jerry <zmeng90@stanford.edu>
2017-05-01 21:47:28 -0700

    Zeroed out ZED's rotation quaternions, TODO disable Oculus sensor

--- ZedUnity2/Assets/ZED/Scripts/ZEDCamera.cs
+++ ZedUnity2/Assets/ZED/Scripts/ZEDCamera.cs
@@ -387,13 +387,15 @@ namespace sl
         public static Quaternion Matrix4ToQuaternion(Matrix4x4 m)
         {
             Quaternion q = new Quaternion();
-            q.w = Mathf.Sqrt(Mathf.Max(0, 1 + m[0, 0] + m[1, 1] + m[2, 2])) / 2;
-            q.x = Mathf.Sqrt(Mathf.Max(0, 1 + m[0, 0] - m[1, 1] - m[2, 2])) / 2;
-            q.y = Mathf.Sqrt(Mathf.Max(0, 1 - m[0, 0] + m[1, 1] - m[2, 2])) / 2;
-            q.z = Mathf.Sqrt(Mathf.Max(0, 1 - m[0, 0] - m[1, 1] + m[2, 2])) / 2;
+            q.w = 0;//Mathf.Sqrt(Mathf.Max(0, 1 + m[0, 0] + m[1, 1] + m[2, 2])) / 2;
+            q.x = 0;//Mathf.Sqrt(Mathf.Max(0, 1 + m[0, 0] - m[1, 1] - m[2, 2])) / 2;
+            q.y = 0;//Mathf.Sqrt(Mathf.Max(0, 1 - m[0, 0] + m[1, 1] - m[2, 2])) / 2;
+            q.z = 0;//Mathf.Sqrt(Mathf.Max(0, 1 - m[0, 0] - m[1, 1] + m[2, 2])) / 2;
+            /*
             q.x *= Mathf.Sign(q.x * (m[2, 1] - m[1, 2]));
             q.y *= Mathf.Sign(q.y * (m[0, 2] - m[2, 0]));
             q.z *= Mathf.Sign(q.z * (m[1, 0] - m[0, 1]));
+            */
             return q;
         }
 
